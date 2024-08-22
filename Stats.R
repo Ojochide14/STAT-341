7dccea6ccc7183581f7631b5c2336ba117ccaf1d
Ojochide14 <115094841+Ojochide14@users.noreply.github.com>
2024-08-22 16:29:57 -0400

    Add files via upload

---
+++ Stats.R
@@ -0,0 +1,14 @@
+#Author: Usman-Aguda, Date:08/22/2024, Purpose: Test the measures of descriptive statistics
+
+#How to calculate standard deviation in R
+x <- c(1,2,3,4,5)
+sd(x)
+
+
+#How to calculate the mean of x in R
+mean(x)
+
+#How to calculate median in R
+median(x)
+
+#How to calculate mode in R
